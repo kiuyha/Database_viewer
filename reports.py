5035b7374d87fbae0fdbb9ed9ba56de6d114f0e5
Kiuyha <ketutshridhara@gmail.com>
2024-12-25 19:31:25 +0700

    initial commit

--- reports.py
+++ reports.py
@@ -1,14 +1,14 @@
 from main import request_api, app, session
 import os
 import requests
+from dotenv import load_dotenv
 
-api_key = 'gAAAAABnZ7M6e0K-mxXauaUUEY942lpLmTY01LU6JbE6YktBr0Jqo6wAjyTO1vfbfgr3jJRyNAf5JYHMaTRB1yuJeknWkSAOL-u2xBRpd70kGaGK6yys2niM8zrQf86S_5K8vehjGDcc'
-
+load_dotenv('.env')
 def request_api(script):
     print(script)
     try:
         url = os.getenv('API_URL')
-        response = requests.post(url, json={'api_key': api_key, 'script': script})
+        response = requests.post(url, json={'api_key': os.getenv('API_KEY'), 'script': script})
         return str(response.text), response.status_code
     except Exception as e:
         return str(e), 500
